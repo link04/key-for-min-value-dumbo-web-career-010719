ad8628ebd5e985b488a15cf9cea62f67c233f26a
Maximo Bautista <max.bautista97@gmail.com>
2018-12-27 05:42:03 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,24 @@
 
 def key_for_min_value(name_hash)
 
+    if name_hash.length > 0
+      number = 0
+      chosen_key = ""
+      
+      name_hash.collect do |key, value|
+        if number == 0
+          number = value
+          chosen_key = key
+        elsif value < number && number != 0
+          number = value
+          chosen_key = key
+        end
+      end
+      
+      chosen_key
+      
+    else
+      nil
+    end
+    
 end
