1882c9c3ee0d3881875023e4d382123fbc42a813
Maximo Bautista <max.bautista97@gmail.com>
2018-12-27 05:36:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,25 +6,11 @@ def key_for_min_value(name_hash)
     if name_hash.length > 0
       
       numero1 = 0
-    
-      
-      numbers = name_hash.collect do |key, value|
-        value
-      end
       
       name_hash.collect do |key, value|
-        
-        numbers.select do |item|
-          if value < item
-            numero1 = value
-            key
-          elsif item < value
-            numero1 = item
-            key
-          end
-        end
-        
+        value
       end
+     
     
     else
       nil
